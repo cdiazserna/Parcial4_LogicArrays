99fe9f6ce349f5cf23e92926c1e289ae6e458f41
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-06-06 18:08:51 -0500

    definimos la estructura a utilizar en este caso el condicional while

---
+++ Punto1Arrays/Punto1Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Punto1Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
