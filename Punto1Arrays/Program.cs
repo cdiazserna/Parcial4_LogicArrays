aec13f02ff11bb33ce27cd691852caa2760884f4
Enmanuelle Perez <emanuel17229@gmail.com>
2023-06-06 18:55:23 -0500

    Avance

--- Punto1Arrays/Program.cs
+++ Punto1Arrays/Program.cs
@@ -6,18 +6,41 @@ namespace Punto1Arrays
     {
         static void Main(string[] args)
         {
+            int tamañoarray, multpnum;
             int menu = 1;
 
             while (menu==1)
             {
+                Console.WriteLine("De qué temaño desea que sea el Array?");
+                tamañoarray = Convert.ToInt32(Console.ReadLine());
 
+                int[] array = new int[tamañoarray];
+
+                Console.WriteLine("De qué número desea optener sus múltiplos?");
+                multpnum = Convert.ToInt32(Console.ReadLine());
+
+                CompletarElArray (array, multpnum);
+                Multiplos (array);
             }
 
         }
 
         static void CompletarElArray (int[] array, int num)
         {
-
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = num * (i + 1);
+            }
         }
+        static void Multiplos (int[] array)
+        {
+            Console.WriteLine("Los múltiplos del número elegido son: ");
+            foreach (int numero in array)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
