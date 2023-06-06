bbbf8c8e9d34813df26a5aec0624c6edb32fa6e6
Andy Morales <andyemmejia@gmail.com>
2023-06-06 18:23:21 -0500

    Commit 2

--- Punto1Arrays/Punto1Arrays/Program.cs
+++ Punto1Arrays/Punto1Arrays/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Punto1Arrays
 {
-    internal class Program
+    internal class Punto1Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size, numero;
+            bool exit = false;
+            string respuesta;
+
+            while (!exit)
+            {
+                Console.WriteLine("Ingrese el tamaño del array:");
+                size = Convert.ToInt32(Console.ReadLine());
+
+                int[] array = new int[size];
+
+                Console.WriteLine("Ingrese el número para generar los múltiplos:");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                
+            }
         }
     }
 }
