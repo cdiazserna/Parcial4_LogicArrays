e93e3dd83f40cf7caacd4b55243b80d4c6f39bdd
JUAN PABLO REBOLLEDO ALMEIDA <juanrebolledo285486@correo.itm.edu.co>
2023-06-06 18:53:22 -0500

    p1

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -6,7 +6,39 @@ namespace Punto1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.WriteLine("Ingrese el tamaño del array:");
+                int tamañoArray = Convert.ToInt32(Console.ReadLine());
+
+                int[] array = new int[tamañoArray];
+
+                Console.WriteLine("Ingrese un número para generar los múltiplos:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                RellenarArrayConMultiplos(array, numero);
+                MostrarMultiplos(array);
+            }
+
+        }
+        static void RellenarArrayConMultiplos(int[] array, int numero)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = numero * (i + 1);
+            }
+        }
+
+
+        static void MostrarMultiplos(int[] array)
+        {
+            Console.WriteLine("Los múltiplos generados son:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
