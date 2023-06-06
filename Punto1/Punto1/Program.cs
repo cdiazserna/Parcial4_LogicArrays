aa2ea772f19ad45ebf9e13b372709286ad715d04
jjtafur <jjtafur22@gmail.com>
2023-06-06 18:45:17 -0500

    Commint3

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -6,37 +6,57 @@ namespace Punto1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese el tamaño del array");
-            int size = Convert.ToInt32(Console.ReadLine());
+            bool exit = false;
 
-            int[] array = new int[size];
+            while (!exit)
+            {
+                Console.WriteLine("Ingrese el tamaño del array");
+                int size = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese un número");
-            int number = Convert.ToInt32(Console.ReadLine());
+                int[] array = new int[size];
 
-            FilloutArray(number, array);
-            ShowResult();
+                Console.WriteLine("Ingrese un número");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                FilloutArray(number, array);
+                ShowResult(array);
+
+                Console.WriteLine("¿Quiere intentarlo de nuevo? (s/n)");
+                string start = Console.ReadLine();
+
+                if (start.ToLower() != "s")
+                {
+                
+                    exit = true;
+                }
+
+                Console.WriteLine();
+            }
+          
+        }
+
+        static void FilloutArray(int number, int[] array)
+
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = number * (i + 1);
+            }
+
+        }
+
+        static void ShowResult(int[] array)
+
+        {
+            Console.WriteLine("Los múltiplos son");
+
+            foreach (int number in array)
+            {
+                Console.WriteLine(number + " ");
+            }
 
             Console.WriteLine();
-        }
-
-        static void FilloutArray(int number, int[] array)
-
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = number * (i + 1);
-            }
-    
-        }
-
-        static void FilloutArray(int number, int[] array)
-
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = number * (i + 1);
-            }
-
+            Console.ReadKey(); 
         }
     }
+}
