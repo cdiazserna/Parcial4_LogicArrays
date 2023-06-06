5f70a292c24890084afa5e5684360568ad2c0306
jjtafur <jjtafur22@gmail.com>
2023-06-06 18:30:10 -0500

    Commint2

--- Punto1/Punto1/Program.cs
+++ Punto1/Punto1/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Punto1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese el tamaño del array");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[size];
+
+            Console.WriteLine("Ingrese un número");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            FilloutArray(number, array);
+            ShowResult();
+
+            Console.WriteLine();
+        }
+
+        static void FilloutArray(int number, int[] array)
+
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = number * (i + 1);
+            }
+    
+        }
+
+        static void FilloutArray(int number, int[] array)
+
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = number * (i + 1);
+            }
+
         }
     }
-}
