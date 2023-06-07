ee72a19e3e3aac4405933a83af0d4d915ce7feca
ricardo taborda palacio <richtp@hotmail.com>
2023-06-06 21:08:02 -0500

    un pequeño avance

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace Punto2Arrays
 {
@@ -6,7 +7,18 @@ namespace Punto2Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenidos al Ahorcadito");
+            Console.WriteLine(" Basado en Marca de Relojes");
+            string[]listword = { "rolex", "yess", "cassio", "ap", "hublot", "tagheuer" };
+            Random random= new Random();
+            string randomword = listword[random.Next(listword.Length)];
+            char[]letters=new char[randomword.Length];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = randomword[i];
+                Console.WriteLine(letters[i]);
+            }
+
         }
     }
 }
