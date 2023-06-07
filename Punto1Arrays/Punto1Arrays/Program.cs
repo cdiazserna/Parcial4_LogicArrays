e5ef7a1ac3e2a6e633555830a286f34849f59956
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-06-06 21:26:03 -0500

    Ejercicio Arrays_Punto1Arrays

--- Punto1Arrays/Punto1Arrays/Program.cs
+++ Punto1Arrays/Punto1Arrays/Program.cs
@@ -2,26 +2,56 @@
 
 namespace Punto1Arrays
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            bool nuevoArray = true;
 
-            while (nuevoArray)
+            int num;
+            bool Nuevoarray = true;
+            
+
+            while (Nuevoarray)
             {
-                Console.Write("Ingrese el tamaño del arreglo: ");
+                Console.Write("Ingrese el tamaño del array: ");
                 int m = Convert.ToInt32(Console.ReadLine());
 
-                int[] array = new int[m];
+                int[] arrayM = new int[m];
 
                 Console.Write("Ingrese un número: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+                num = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("¿Desea conocer los multiplos de otro número? (Si/No)");
+                Multiplos(arrayM, num);
+                mostrarMultiplos(arrayM);
+
+                Console.WriteLine("¿Desea crear otro multiplo? (si/no)");
                 string opcion = Console.ReadLine();
-                nuevoArray = (opcion.ToLower() == "Si");
+                Nuevoarray = (opcion.ToLower() == "si");
+
+
             }
+        }
+
+        static void Multiplos(int[] arrayM, int num)
+        {
+            for (int i = 0; i < arrayM.Length; i++)
+            {
+                arrayM[i] = num * (i + 1);
+            }
+        }
+
+        static void mostrarMultiplos(int[] arrayM)
+        {
+            Console.WriteLine("Los múltiplos del arreglo son:");
+            for (int i = 0; i < arrayM.Length; i++)
+            {
+                Console.WriteLine(arrayM[i]);
+            }
+
+            
         }
     }
 }
+
+
+    
