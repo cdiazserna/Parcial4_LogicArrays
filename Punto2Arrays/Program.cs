9182ded148b1f8bbe666cf80ab7c03d72c0312ca
Enmanuelle Perez <emanuel17229@gmail.com>
2023-06-06 19:32:34 -0500

    Avance

---
+++ Punto2Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Punto2Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listWords =
+            {
+                "tvs, bmw, bajaj, honda, suzuki, akt, hero, victory, yamaha, royalenfild, kawasaki"
+            };
+            Random random= new Random();
+
+            string randomWord = listWords[random.Next(listWords.Length)];
+            
+        }
+    }
+}
