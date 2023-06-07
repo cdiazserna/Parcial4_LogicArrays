3b9b21fda3a0ba7e26435a46670db4b6ad6d087f
Enmanuelle Perez <emanuel17229@gmail.com>
2023-06-06 20:50:28 -0500

    Avance

--- Punto2Arrays/Program.cs
+++ Punto2Arrays/Program.cs
@@ -10,10 +10,41 @@ namespace Punto2Arrays
             {
                 "tvs, bmw, bajaj, honda, suzuki, akt, hero, yamaha, royalenfild, kawasaki"
             };
+
             Random random= new Random();
 
             string randomWord = listWords[random.Next(listWords.Length)];
-            
+
+            int intentosMaximos = 5;
+            int intentosRealizados = 0;
+            bool juegoTerminado = false;
+
+            string[] letrasPalabraSecreta = new string[randomWord.Length];
+            for (int i = 0; i < randomWord.Length; i++)
+            {
+                letrasPalabraSecreta[i] = randomWord[i].ToString();
+            }
+
+            string[] palabraAdivinada = new string[randomWord.Length];
+            for (int i = 0; i < palabraAdivinada.Length; i++)
+            {
+                palabraAdivinada[i] = "_";
+            }
+
+            Console.WriteLine("¡Bienvenido al juego del Ahorcado!");
+            while (!juegoTerminado)
+            {
+                Console.WriteLine("\nPalabra: " + String.Join(" ", palabraAdivinada));
+                Console.WriteLine("Intentos restantes: " + (intentosMaximos - intentosRealizados));
+
+                Console.Write("Ingresa una letra: ");
+                string letra = Console.ReadLine().ToLower();
+
+                
+            }
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
