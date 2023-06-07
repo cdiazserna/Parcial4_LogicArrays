054ac484b190fab137435f3858519462a44e6b33
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-06-06 21:57:58 -0500

    regreso al menu

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -7,87 +7,101 @@ namespace Punto2Arrays
         static void Main(string[] args)
         {
             string[] listWords = { "rojo", "verde", "amarillo", "azul", "violeta", "naranja", "marron", "negro", "lila", "rosado" };
-            Random random = new Random();
-            char[] randomWord = listWords[random.Next(listWords.Length)];
+            //Random random = new Random();
+            string randomWord = selectrandomWord(listWords);
             // almacenamos la paralbra en uun array
             char[] gessedLetters = InitgessedLetters(randomWord.Length);//tenemos que definir un metodo para inicializar 
-
+            char[] lettersWord = getLettersword(randomWord);
             // utilizamos while como condicional
             int triesRemaining = 5;
-
-            while (triesRemaining > 0)
+            bool continu = true;
+            while (continu)
             {
-                Console.WriteLine("Gess the word:");
-                Console.WriteLine(gessedLetters);//usaremos el metodo gessedLetters
-
-                Console.WriteLine("Enter a letter:");
-                char letter = char.ToLower(Console.ReadKey().KeyChar);
-
-                bool letterFound = updtgessedLetters(letter,randomWord, gessedLetters);
-                if (letterFound)
+                while (triesRemaining > 0)
                 {
-                    if (areEqual(gessedLetters, randomWord))
+                    Console.WriteLine("Gess the word:");
+                    Console.WriteLine(gessedLetters);//usaremos el metodo gessedLetters
+
+                    Console.WriteLine("Enter a letter:");
+                    char letter = char.ToLower(Console.ReadKey().KeyChar);
+
+                    bool letterFound = updtgessedLetters(letter, lettersWord, gessedLetters);
+                    if (letterFound)
                     {
-                        Console.WriteLine("\n¡winner!");
-                        return;
+                        if (areEqual(gessedLetters, lettersWord))
+                        {
+                            Console.WriteLine("\n¡winner!'{randomWord}'");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        triesRemaining--;
+                        Console.WriteLine($"\n you have {triesRemaining} tries to guess the word.");
+                    }
+
+                }
+                Console.WriteLine($"\nYou lose the word is '{randomWord}'.");
+
+                Console.WriteLine("Do you want to continue? (S/N)");
+                string opcion = Console.ReadLine().ToUpper();
+
+                if (opcion == "N")
+                {
+                    continu = false;
+                }
+
+
+
+            }
+
+
+            static string selectrandomWord(string[] words)
+            {
+                Random random = new Random();
+                int RandomIndex = random.Next(words.Length);
+                return words[RandomIndex];
+            }
+
+            static char[] getLettersword(string word)
+            {
+                return word.ToCharArray();
+            }
+
+            static char[] InitgessedLetters(int length)
+            {
+                char[] gessedLetters = new char[length];
+                for (int i = 0; i < gessedLetters.Length; i++)
+                {
+                    gessedLetters[i] = '_';
+                }
+                return gessedLetters;
+            }
+            static bool updtgessedLetters(char letter, char[] lettersWord, char[] lettersGessed)
+            {
+                bool letterFound = false;
+                for (int i = 0; i < lettersWord.Length; i++)
+                {
+                    if (lettersWord[i] == letter)
+                    {
+                        lettersGessed[i] = letter;
+                        letterFound = true;
                     }
                 }
-                else
-                {
-                    triesRemaining--;
-                    Console.WriteLine($"\n you have {triesRemaining} tries to guess the word.");
-                }
-
+                return letterFound;
             }
-            Console.WriteLine($"\nYou lose the word is '{randomWord}'.");
-        }
-
-
-        //static string selectrandomWord(string[] words)
-        //        {
-        //            Random random = new Random();
-        //            int indiceAleatorio = random.Next(words.Length);
-        //            return words[RandomIndex];
-        //            }
-
-        static char[] getLettersword(string word)
-        {
-            return word.ToCharArray();
-        }
-
-        static char[] InitgessedLetters(int length)
-        {
-            char[] gessedLetters = new char[length];
-            for (int i = 0; i < gessedLetters.Length; i++)
+            static bool areEqual(char[] array1, char[] array2)
             {
-                gessedLetters[i] = '_';
-            }
-            return gessedLetters;
-        }
-        static bool updtgessedLetters(char letter, char[] lettersWord, char[] lettersGessed)
-        {
-            bool letterFound = false;
-            for (int i = 0; i < lettersWord.Length; i++)
-            {
-                if (lettersWord[i] == letter)
-                {
-                    lettersGessed[i] = letter;
-                    letterFound = true;
-                }
-            }
-            return letterFound;
-        }
-        static bool areEqual(char[] array1, char[] array2)
-        {
-            if (array1.Length != array2.Length)
-                return false;
-            for (int i = 0; i < array1.Length; i++)
-            {
-                if (array1[i] != array2[i])
+                if (array1.Length != array2.Length)
                     return false;
-            }
-            return true;
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    if (array1[i] != array2[i])
+                        return false;
+                }
+                return true;
 
+            }
         }
     }
 }
