fd10097617e89b07f679c2c7489bca8a3e99f9e0
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-06-06 22:25:06 -0500

    Fin

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -12,58 +12,111 @@ namespace Punto2Arrays
                 Random random = new Random();
                 string randomWord = listWords[random.Next(listWords.Length)];
 
-                char[] incognita = InitializeIncognita(randomWord.Length);
+                char[] incognita = InicializarIncognita(randomWord.Length);
                 int intentos = 5;
 
                 Console.WriteLine("¡El Juego del Ahorcado!");
-                Console.WriteLine("Intente adivinar la palabra:");
+                Console.WriteLine("Adivine la palabra:");
                 PrintIncognita(incognita);
 
-                while (intentos > 0 && !IsWordGuessed(incognita))
+                while (intentos > 0 && !IsAdivinarLetra(incognita))
                 {
-                    Console.WriteLine("\nIngresa una letra: ");
+                    Console.WriteLine("\nIngrese una letra: ");
                     string input = Console.ReadLine();
 
                     if (input.Length == 1)
                     {
-                        char guessedLetter = input[0];
+                        char Letra = input[0];
 
-                        bool isLetterInWord = UpdateIncognita(guessedLetter, randomWord, incognita);
+                        bool isCorrecto = ActualizarIncognita(Letra, randomWord, incognita);
 
-                        if (isLetterInWord)
+                        if (isCorrecto)
                         {
-                            Console.WriteLine("\n¡Correcto! La letra está en la palabra.");
+                            Console.WriteLine("\n¡Correcto!");
                         }
                         else
                         {
                             intentos--;
-                            Console.WriteLine("\nIncorrecto. Te quedan {0} cartuchos para adivinar la palabra.", intentos);
+                            Console.WriteLine("\nIncorrecto. Te quedan {0} intentos para adivinar la palabra.", intentos);
                         }
 
                         PrintIncognita(incognita);
                     }
                     else
                     {
-                        Console.WriteLine("\nDebes ingresar una única letra. Intenta de nuevo.");
+                        Console.WriteLine("\nDebe ingresar una única letra. Intente de nuevo.");
                     }
                 }
 
-                if (IsWordGuessed(incognita))
+                if (IsAdivinarLetra(incognita))
                 {
-                    Console.WriteLine("\n¡Has ganado!");
+                    Console.WriteLine("\n¡Ha ganado!");
                 }
                 else
                 {
-                    Console.WriteLine("\nHas perdido. La palabra era: '{0}'.", randomWord);
+                    Console.WriteLine("\nHa perdido. La palabra era: '{0}'.", randomWord);
                 }
 
-                Console.WriteLine("\n¿Quieres jugar de nuevo? (s/n)");
-            } while (Console.ReadLine().ToLower() == "s");
+                Console.WriteLine("\n¿Quiere jugar de nuevo? (si/no)");
+            } while (Console.ReadLine().ToLower() == "si");
 
-            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
 
         }
+
+        static char[] InicializarIncognita(int length)
+        {
+            char[] incognita = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                incognita[i] = '_';
+            }
+
+            return incognita;
+        }
+
+        static void PrintIncognita(char[] incognita)
+        {
+            Console.WriteLine();
+
+            foreach (char c in incognita)
+            {
+                Console.Write(c + " ");
+            }
+        }
+
+        static bool ActualizarIncognita(char NuevaLetra, string randomWord, char[] incognita)
+        {
+            bool isLetraCorrecta = false;
+
+            for (int i = 0; i < randomWord.Length; i++)
+            {
+                if (randomWord[i] == NuevaLetra)
+                {
+                    incognita[i] = NuevaLetra;
+                    isLetraCorrecta = true;
+                }
+            }
+
+            return isLetraCorrecta;
+        }
+
+        static bool IsAdivinarLetra(char[] incognita)
+        {
+            foreach (char c in incognita)
+            {
+                if (c == '_')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
-        
-    }
+}
+
+
+  
 
