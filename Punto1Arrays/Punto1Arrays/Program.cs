da31a01e3fd321c35c471ab4685977cc39dd45e8
ricardo taborda palacio <richtp@hotmail.com>
2023-06-06 19:39:03 -0500

    proceso de relleno

--- Punto1Arrays/Punto1Arrays/Program.cs
+++ Punto1Arrays/Punto1Arrays/Program.cs
@@ -6,7 +6,27 @@ namespace Punto1Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Punto 1:Crea un array donde el usuario le indique el tamaño por teclado.
+            //Crear un subprograma que rellene el array con los múltiplos de un número pedido por teclado.
+            //Por ejemplo, si defino un array de tamaño 5 y elijo un 3 en el subprograma,
+            //el array contendrá(debe comenzar desde el número ingresado):3691215Muestra
+            //los  múltiplos  resultantes  por  pantalla  usando  otro  subprograma, Además,
+            //el algoritmo debe estar en la capacidad de volver al inicio para pedir un nuevo array y número.
+            int stwh = 1;
+
+            while(stwh == 1)
+            {
+                EnterData();
+
+            }
+
+        }
+        public static void EnterData()
+        {
+            int n;
+            Console.WriteLine(" por favor introduce el tamaño del recuadro");
+            n=Convert.ToInt32(Console.ReadLine());
+            int[] Stuffed=new int[n];
         }
     }
 }
