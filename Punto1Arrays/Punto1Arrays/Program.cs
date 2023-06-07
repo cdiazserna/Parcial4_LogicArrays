9ff9b37dcd197fe5faaef28999a97df369428acb
ricardo taborda palacio <richtp@hotmail.com>
2023-06-06 20:11:01 -0500

    terminado

--- Punto1Arrays/Punto1Arrays/Program.cs
+++ Punto1Arrays/Punto1Arrays/Program.cs
@@ -12,21 +12,50 @@ namespace Punto1Arrays
             //el array contendrá(debe comenzar desde el número ingresado):3691215Muestra
             //los  múltiplos  resultantes  por  pantalla  usando  otro  subprograma, Además,
             //el algoritmo debe estar en la capacidad de volver al inicio para pedir un nuevo array y número.
-            int stwh = 1;
-
+            int stwh = 1,n;
+            int[] stuffed;
             while(stwh == 1)
             {
-                EnterData();
-
+                EnterData(out n,out stuffed);
+                stuffed=ArrayPadding( n, stuffed);
+                ShowResults(stuffed,out stwh);
             }
 
         }
-        public static void EnterData()
+        public static void EnterData(out int n, out int[] Stuffed)
         {
-            int n;
+          
             Console.WriteLine(" por favor introduce el tamaño del recuadro");
             n=Convert.ToInt32(Console.ReadLine());
-            int[] Stuffed=new int[n];
+             Stuffed=new int[n];
+        }
+        public static int[] ArrayPadding(int n,int[] STuffed)
+        {
+            
+            STuffed = new int[n];
+            
+            Console.WriteLine("por favor ingresa el numero que deseas ver los multiplos");
+            STuffed[0]= Convert.ToInt32(Console.ReadLine());
+            
+            for (int i=1;i<STuffed.Length;i++) 
+            {
+                STuffed[i] = STuffed[0] * (i + 1);
+                
+            }
+            return STuffed;
+            
+        }
+        public static void ShowResults(int[] stuffed,out int stwh)
+        {
+            Console.WriteLine("\n los multimplos son : \n");
+            for (int i = 0; i < stuffed.Length; i++)
+            {
+                
+                Console.WriteLine(stuffed[i]);
+            }
+            Console.WriteLine("si deseas introducir otro numero marca 1 o cualquiera para finalizar");
+            stwh= Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
         }
     }
 }
