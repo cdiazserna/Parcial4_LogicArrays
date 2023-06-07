662be6e6a00ef94ac58b67e0e8c5f11f713163d7
Andy Morales <andyemmejia@gmail.com>
2023-06-06 20:00:27 -0500

    Commit 4

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -5,13 +5,10 @@ namespace Punto2Arrays
     public class Punto2Arrays
     {
         static string[] listWords = { "Perro", "Gato", "Cocodrilo", "Guepardo", "Gorilla", "Delfin", "Murcielago", "Mapache", "Escarabajo", "Jirafa" };
-        Random random = new Random();
         static char[] palabraAleatoria;
         static int cartuchosRestantes;
         static void Main(string[] args)
         {
-            //string randomWord = listWords[random.Next(listWords.Length)];
-
             bool jugarDeNuevo = true;
 
             while (jugarDeNuevo)
@@ -27,14 +24,60 @@ namespace Punto2Arrays
         }
         static void SeleccionarPalabraAleatoria()
         {
-            Random rnd = new Random();
-            int indiceAleatorio = rnd.Next(0, listWords.Length);
-            palabraAleatoria = listWords[indiceAleatorio].ToCharArray();
+            Random random = new Random();
+            int randomWord = random.Next(0, listWords.Length);
+            palabraAleatoria = listWords[randomWord].ToCharArray();
         }
 
         static void InicializarJuego()
         {
             cartuchosRestantes = 5;
+        }
+
+        static void Jugar()
+        {
+            bool[] letrasDescubiertas = new bool[palabraAleatoria.Length];
+
+            while (cartuchosRestantes > 0 && Array.IndexOf(letrasDescubiertas, false) != -1)
+            {
+                Console.WriteLine("Palabra: " + (letrasDescubiertas));
+                Console.WriteLine("Te quedan " + cartuchosRestantes + " cartuchos para adivinar la palabra.");
+                Console.Write("Ingresa una letra: ");
+                char letra = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                bool letraEncontrada = false;
+
+                for (int i = 0; i < palabraAleatoria.Length; i++)
+                {
+                    if (palabraAleatoria[i] == letra)
+                    {
+                        letrasDescubiertas[i] = true;
+                        letraEncontrada = true;
+                    }
+                }
+
+                if (letraEncontrada)
+                {
+                    Console.WriteLine("¡Letra encontrada!");
+                }
+                else
+                {
+                    cartuchosRestantes--;
+                    Console.WriteLine("La letra no está en la palabra.");
+                }
+
+                Console.WriteLine();
+            }
+
+            if (cartuchosRestantes == 0)
+            {
+                Console.WriteLine("Has perdido, la palabra era '" + new string(palabraAleatoria) + "'.");
+            }
+            else
+            {
+                Console.WriteLine("¡Has ganado!");
+            }
         }
 
 
