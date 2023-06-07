b113e05fd93a590bf696280398c4fc6a09c56a8b
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-06-06 21:58:32 -0500

    Seguimiento

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -6,15 +6,11 @@ namespace Punto2Arrays
     {
         static void Main(string[] args)
         {
-                do
-                {
+                
                     string[] listWords = { "papas", "hijos", "hermanos", "tios", "sobrinos", "primos", "abuelos", "nietos" };
                     Random random = new Random();
                     string randomWord = listWords[random.Next(listWords.Length)];
-                } while (Console.ReadLine().ToLower() == "s");
-
-                Console.WriteLine("\nPresiona cualquier tecla para salir...");
-
+             
          }
         
     }
