b3848928cbcada122d8f962a391677bad2ea8c5f
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-06-06 20:28:40 -0500

    primero hay que seleccionar ua palabra aleatoria

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -10,12 +10,32 @@ namespace Punto2Arrays
             Random random = new Random();
             string randomWord = listWords[random.Next(listWords.Length)];
             // almacenamos la paralbra en uun array
-          // char[] 
+            char[] gessedLetters = InitgessedLetters(randomWord.Length);//tenemos que definir un metodo para inicializar 
 
             // utilizamos while como condicional
-            while (true)
-            {
+            int triesRemaining = 5;
 
+            while (triesRemaining > 0)
+            {
+                Console.WriteLine("Gess the word:");
+                Console.WriteLine(gessedLetters);//usaremos el metodo gessedLetters
+
+                Console.WriteLine("Enter a letter:");
+                char letter = char.ToLower(Console.ReadKey().KeyChar);
+
+
+
+                static char[] InitgessedLetters(int longitud)
+                {
+                    char[] gessedLetters = new char[longitud];
+                    for (int i = 0; i < gessedLetters.Length; i++)
+                    {
+                        gessedLetters[i] = '_';
+                    }
+                    return gessedLetters;
+                }
+              
+                }
             }
         }
     
