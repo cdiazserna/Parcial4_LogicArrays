c03a889173885f6c1129442dd937b2bf0dc3e1cc
LEYDI YULIETH PUERTA CAMPO <leydipuerta249947@correo.itm.edu.co>
2023-06-06 21:02:46 -0500

    Ejercicio

---
+++ Punto1Arrays/Punto1Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Punto1Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool nuevoArray = true;
+
+            while (nuevoArray)
+            {
+                Console.Write("Ingrese el tamaño del arreglo: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+
+                int[] array = new int[m];
+
+                Console.Write("Ingrese un número: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("¿Desea conocer los multiplos de otro número? (Si/No)");
+                string opcion = Console.ReadLine();
+                nuevoArray = (opcion.ToLower() == "Si");
+            }
+        }
+    }
+}
