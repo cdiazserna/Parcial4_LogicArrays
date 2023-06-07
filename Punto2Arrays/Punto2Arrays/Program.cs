497198337b3eac76e8f4689e45d0880bbb87484f
Andy Morales <andyemmejia@gmail.com>
2023-06-06 19:23:34 -0500

    Commit 1

---
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Punto2Arrays
+{
+    public class Punto2Arrays
+    {
+        static void Main(string[] args)
+        {
+            string[] listWords = { "Perro", "Gato", "Cocodrilo", "Guepardo", "Gorilla", "Delfin", "Murcielago", "Mapache", "Escarabajo", "Jirafa" };
+            Random random = new Random();
+
+            string randomWord = listWords[random.Next(listWords.Length)];
+
+        }
+    }
+}
