7e94637c8e0534b91cb2ab432372b061ed6613c9
JAIRO LEANDRO MORA GUERRA <jairomora214411@correo.itm.edu.co>
2023-06-06 21:09:07 -0500

    corrigiendo errores

--- Punto2Arrays/Punto2Arrays/Program.cs
+++ Punto2Arrays/Punto2Arrays/Program.cs
@@ -24,42 +24,74 @@ namespace Punto2Arrays
                 char letter = char.ToLower(Console.ReadKey().KeyChar);
 
                 bool letterFound = updtgessedLetters(letter, lettersWord, lettersGessed);
-
-
-
-
-
-                static string selectrandomWord(string[] words)
+                if (letterFound)
                 {
-                    Random random = new Random();
-                    int indiceAleatorio = random.Next(words.Length);
-                    return words[RandomIndex];
-                }
-
-                static char[] InitgessedLetters(int longitud)
-                {
-                    char[] gessedLetters = new char[longitud];
-                    for (int i = 0; i < gessedLetters.Length; i++)
+                    if (areEqual(lettersGessed, lettersWord))
                     {
-                        gessedLetters[i] = '_';
+                        Console.WriteLine("\n¡winner!");
+                        return;
                     }
-                    return gessedLetters;
                 }
-                static bool updtgessedLetters(char letter, char[] lettersWord, char[] lettersGessed)
+                else
                 {
-                    bool letterFound = false;
-                    for (int i = 0; i < lettersWord.Length; i++)
-                    {
-                        if (lettersWord[i] == letter)
-                        {
-                            lettersGessed[i] = letter;
-                            letterFound = true;
-                        }
-                    }
-                    return letterFound;
+                    triesRemaining--;
+                    Console.WriteLine($"\n you have {triesRemaining} tries to guess the word.");
                 }
+
             }
-            }
+            Console.WriteLine($"\nYou lose the word is '{randomWord}'.");
         }
-    
+
+
+        //static string selectrandomWord(string[] words)
+        //        {
+        //            Random random = new Random();
+        //            int indiceAleatorio = random.Next(words.Length);
+        //            return words[RandomIndex];
+        //            }
+
+        static char[] getLettersword(string word)
+        {
+            return word.ToCharArray();
+        }
+
+        static char[] InitgessedLetters(int length)
+        {
+            char[] gessedLetters = new char[length];
+            for (int i = 0; i < gessedLetters.Length; i++)
+            {
+                gessedLetters[i] = '_';
+            }
+            return gessedLetters;
+        }
+        static bool updtgessedLetters(char letter, char[] lettersWord, char[] lettersGessed)
+        {
+            bool letterFound = false;
+            for (int i = 0; i < lettersWord.Length; i++)
+            {
+                if (lettersWord[i] == letter)
+                {
+                    lettersGessed[i] = letter;
+                    letterFound = true;
+                }
+            }
+            return letterFound;
+        }
+        static bool areEqual(char[] array1, char[] array2)
+        {
+            if (array1.Length != array2.Length)
+                return false;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    return false;
+            }
+            return true;
+
+        }
+    }
 }
+            
+        
+    
+
